1c5b13bc56767b2d658e6634706a5cdc97e8601c
Dennis <thecvcoder@foxmail.com>
2024-05-30 21:13:14 +0800

    change:<Brand /> font-size

--- components/brand.tsx
+++ components/brand.tsx
@@ -1,10 +1,11 @@
-import { Icons } from "./icons"
+import { Icons } from './icons'
 
 export const Brand: React.FC = () => {
   return (
     <h1>
-      <a href="/" className="flex text-xl gap-1 items-center mr-[25px]">
-        <Icons.logo />oh my link
+      <a href="/" className="flex gap-1 items-center mr-[25px]">
+        <Icons.logo className="text-2xl" />
+        <span className="text-xl">oh my link</span>
       </a>
     </h1>
   )
